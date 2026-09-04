f21c19e7cc70dcc48afffab588a20e5dfab81c80
OULAI LI <82217503+Liolai@users.noreply.github.com>
2026-09-04 17:28:55 +0200

    revert

--- tester/trex/trex_run.sh
+++ tester/trex/trex_run.sh
@@ -9,4 +9,4 @@ fi
 if pgrep -f "t-rex-64" > /dev/null; then
 	sudo kill $(pgrep -f "t-rex-64")
 fi
-sh -c "cd /opt/trex-core-3.08 && sudo nohup ./t-rex-64 -i -c $(($(nproc) - 2)) --iom 0 > /tmp/trex.log 2>&1 &" > /dev/null
+sh -c "cd /opt/trex-core-3.08 && sudo nohup ./t-rex-64 -i -c 7 --iom 0 > /tmp/trex.log 2>&1 &" > /dev/null
